e606964393cae65154cab866b8f6d9b8f4cf2e07
fatahrez <fertahrez@gmail.com>
2020-01-27 13:04:12 +0300

    initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/fatah/Documents/ProjectsProduction/Tremy/TremyServer/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
