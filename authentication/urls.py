e60765cbc97149de4ee493d06e0c5ea8a8c4494b
fatahrez <fertahrez@gmail.com>
2020-01-28 01:09:11 +0300

    in authentication/urls.py add user endpoint to Retrieve and update user

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,4 +5,5 @@ from authentication import views
 urlpatterns = [
     path('register', views.RegistrationAPIView.as_view(), name="register"),
     path('login', views.LoginAPIView.as_view(), name="login"),
+    path('user', views.UserRetrieveUpdateAPIView.as_view(), name="user")
 ]
