48baa574ff5ed7acccecb30d44d2f5ee1aea4794
fatahrez <fertahrez@gmail.com>
2020-01-28 11:07:58 +0300

    add Profile model in appprofile/models.py

--- TremyServer/settings.py
+++ TremyServer/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'authentication.apps.AuthenticationConfig',
+    'appprofile.apps.AppprofileConfig',
 ]
 
 MIDDLEWARE = [
--- appprofile/models.py
+++ appprofile/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    bio = models.CharField(blank=True, max_length=500)
+    image = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.user.email
