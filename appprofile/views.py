ffb3e1a3e54efd1334186728c438feb690a11372
fatahrez <fertahrez@gmail.com>
2020-01-28 12:08:02 +0300

    in appprofile/views.py add classes ProfileView() and ProfileDetail() to handle user input

--- appprofile/views.py
+++ appprofile/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+
+from rest_framework.views import APIView
+
+from appprofile.models import Profile
 
 # Create your views here.
+from appprofile.serializers import ProfileSerializer, ProfileSerializerUpdate
+from authentication.serializers import LoginSerializer
+
+
+class ProfileView(APIView):
+
+    @staticmethod
+    def get():
+        profiles = Profile.objects.all()
+        return Response(ProfileSerializer(profiles, many=True).data)
+
+
+class ProfileDetail(APIView):
+
+    @staticmethod
+    def patch(request, profile_id):
+        profile = get_object_or_404(Profile, pk=profile_id)
+
+        if profile.user != request.user:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        serializer = ProfileSerializerUpdate(profile, data=request.data, context={'request': request}, partial=True)
+        if serializer.is_valid():
+            profile = serializer.save()
+            return Response(LoginSerializer(profile.user).data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
