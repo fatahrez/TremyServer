cc5663bbbff4c8a5b51b5a1c59b5e605862fd4c5
fatahrez <fertahrez@gmail.com>
2020-01-28 12:17:43 +0300

    create app core to handle shared stuff for app

--- appprofile/urls.py
+++ appprofile/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from appprofile import views
 
 urlpatterns = [
-    path('$', views.ProfileView.as_view()),
+    path('/', views.ProfileView.as_view()),
     path('/<int:profile_id>$', views.ProfileDetail.as_view()),
 ]
