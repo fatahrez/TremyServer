a139099410352f8c98508229a11cc3ba8731898f
fatahrez <fertahrez@gmail.com>
2020-01-28 11:25:14 +0300

    in apps.py and __init__.py help initiate signals.py to pick user creation

--- authentication/apps.py
+++ authentication/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class AuthenticationConfig(AppConfig):
     name = 'authentication'
+    verbose_name = _('appprofile')
+
+    def ready(self):
+        import authentication.signals
