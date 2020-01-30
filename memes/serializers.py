6ae65e0f5bd22005bbd32a581edcee5d131aff5a
fatahrez <fertahrez@gmail.com>
2020-01-30 22:20:57 +0300

    in memes/serializers.py add class MemeSerializerCreate to create a new meme, it also uses HiddenField for serializer to get current user

--- memes/serializers.py
+++ memes/serializers.py
@@ -1,11 +1,20 @@
-from authentication import serializers
+from rest_framework import serializers
 from authentication.serializers import UserSerializer
 from memes.models import Meme
 
 
-class MemesSerializer(serializers.ModelSerializer):
+class MemeSerializer(serializers.ModelSerializer):
     user = UserSerializer()
 
     class Meta:
         model = Meme
         fields = '__all__'
+
+
+class MemeSerializerCreate(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Meme
+        fields = '__all__'
+
