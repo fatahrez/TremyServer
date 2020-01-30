3673de937022c9f03b60f5f61646bc70bdf8734f
fatahrez <fertahrez@gmail.com>
2020-01-30 22:09:13 +0300

    create model Meme in memes/models.py

--- memes/models.py
+++ memes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from TremyServer import settings
+from core.models import CreatedModified
+
+
 # Create your models here.
+class Meme(CreatedModified):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    body = models.CharField(max_length=300)
+    image = models.ImageField(blank=True)
+
+    class Meta:
+        default_related_name = 'memes'
+
+    def __str__(self):
+        return self.body
